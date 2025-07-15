019e30fc56a925b21f566af314d6221f89ba4840
progrmaingnpc <imperialproductions00@gmail.com>
2025-07-15 16:10:39 +0000

    adding an install script

---
+++ autoconfigure.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+yay -Syu hyprland hyprpaper swww hyprlock waybar \
+       xdg-desktop-portal xdg-desktop-portal-hyprland waypaper \
+       hypridle hyprpicker hyprland-qt-support hyprland-qtutils \
+
