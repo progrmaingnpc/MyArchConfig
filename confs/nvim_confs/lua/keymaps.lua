5b2ea05469dd7f48a99fba8d655a4230f43602b0
progrmaingnpc <imperialproductions00@gmail.com>
2025-07-27 21:29:58 +0300

    adding neovim config

--- confs/nvim_confs/lua/keymaps.lua
+++ confs/nvim_confs/lua/keymaps.lua
@@ -1,2 +1,5 @@
 vim.g.mapleader = " "
 vim.keymap.set('n', '<Leader>w', '<cmd>echo "Example 1"<cr>')
+vim.keymap.set({ 'n', 'v' }, '<C-v>', '"+p')
+vim.keymap.set({ 'n', 'v' }, '<C-x>', '"+d')
+vim.keymap.set({ 'n', 'v' }, '<C-c>', '"+y')
