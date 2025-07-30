37a4791df72868fee0dd1416224506519fc519c6
progrmaingnpc <imperialproductions00@gmail.com>
2025-07-30 17:17:59 +0300

    adding neovim config

--- confs/nvim_confs/lua/keymaps.lua
+++ confs/nvim_confs/lua/keymaps.lua
@@ -16,6 +16,9 @@ vim.keymap.set({ 'n', 'v' }, '<Leader>E', ':%s/')
 --]=====]
 vim.keymap.set({ 'n', 'v' }, '<Leader>e', ':%s///gc')
 -- Select all occurances of a word pattern your cursor is on(press n to move to the next occurence)
-vim.keymap.set({ 'n', 'v' }, '<Leader>y', '"*')
+vim.keymap.set({ 'n', 'v' }, '<Leader>y', '"+*')
 --[=====[ Code navigation --]=====]
 vim.keymap.set({ 'n', 'v' }, '<Leader>t', ':ij ')
+--[=====[ Listings--]=====]
+-- List all recorded registers entered by the user
+vim.keymap.set({ 'n', 'v' }, '<Leader>1', '":reg')
