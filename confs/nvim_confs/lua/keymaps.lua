1355ca649340a9c983c1a162bf596755cc0b3d3a
progrmaingnpc <imperialproductions00@gmail.com>
2025-07-30 17:26:53 +0300

    adding neovim config

--- confs/nvim_confs/lua/keymaps.lua
+++ confs/nvim_confs/lua/keymaps.lua
@@ -16,7 +16,7 @@ vim.keymap.set({ 'n', 'v' }, '<Leader>E', ':%s/')
 --]=====]
 vim.keymap.set({ 'n', 'v' }, '<Leader>e', ':%s///gc')
 -- Select all occurances of a word pattern your cursor is on(press n to move to the next occurence)
-vim.keymap.set({ 'n', 'v' }, '<Leader>y', '"*')
+vim.keymap.set({ 'n', 'v' }, '<Leader>y', '"+*')
 --[=====[ Code navigation --]=====]
 vim.keymap.set({ 'n', 'v' }, '<Leader>t', ':ij ')
 --[=====[ Listings--]=====]
