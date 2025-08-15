6145b172342691044622495f85d5776bbde5f036
progrmaingnpc <imperialproductions00@gmail.com>
2025-08-15 23:00:10 +0300

    removing duplicates

--- autoconfigure.sh
+++ autoconfigure.sh
@@ -24,16 +24,16 @@ if [[ ! -d "$YAY_DIRECTORY" ]] && [[ ! -d "$PARU_DIRECTORY" ]]; then
 fi
 
 if [ -d "$YAY_DIRECTORY" ]; then
-    AUR_MANAGER=yay
+    AUR_MANAGER=$(yay)
 	echo "There is a yay cache at $YAY_DIRECTORY"
 
 elif [	-d "$PARU_DIRECTORY" ]; then
-    AUR_MANAGER=paru
+    AUR_MANAGER=$(paru)
 	echo "There is a paru cache at $PARU_DIRECTORY"
 fi
 
-echo $AUR_MANAGER -Syu
-$(AUR_MANAGER) -Syu
+#echo $AUR_MANAGER -Syu
+$AUR_MANAGER -Syu
 exit
 
 AUR_MANAGER -Syu
