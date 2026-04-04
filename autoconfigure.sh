470119ed069703644444104c211e38c8ac9090ed
progrmaingnpc <imperialproductions00@gmail.com>
2026-04-04 20:34:15 +0300

    reflector setup

--- autoconfigure.sh
+++ autoconfigure.sh
@@ -99,7 +99,7 @@ echo "[Successfully installed keyring and kleopatra]"
 $AUR_MANAGER -S ttf-dejavu ttf-liberation noto-fonts noto-fonts-emoji ttf-font-awesome ttf-material-symbols-variable nerd-fonts --noconfirm --needed
 echo "[Successfully installed fonts]"
 
-$AUR_MANAGER -S yazi fd resvg xsel 7zip
+$AUR_MANAGER -S yazi fd resvg xsel 7zip --noconfirm --needed
 echo "[Successfully installed yazi]"
 
 # Create the hyprland directory (hypr) if it doesn't already exist
